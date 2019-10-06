d2ebf6242e2d19711e842c5364af613d46d0c1f4
Valera <yarovoy.valera2002@gmail.com>
2019-10-06 09:46:59 +0000

    first commit

---
+++ lesson_1/name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//#include "iostream"
+//#include "iostream.h" .hpp
+
+//using namespace std;
+
+int main(){
+    // cout << "hello,world!\n"
+    std::cout <<"Hello,world!\n";
+    return 0;
+}
---
+++ lesson_2/types.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number1 = 4;
+    int number2 = 5;
+    cout << number1<<"+"<< number2<<"="<<number1+number2<< endl;
+
+}
