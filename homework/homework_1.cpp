c71aa2d92026cbdaa76f87c84c3e8ca137c58de9
Valera <yarovoy.valera2002@gmail.com>
2019-11-19 17:43:10 +0000

    homework

---
+++ homework/homework_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (char x = 65; x<=90 ; x++)
+    {
+    cout<<x<<endl;
+    }
+    for (char x =97 ; x<=122 ; x++)
+    {
+    cout<<x<<endl;
+    }
+    return 0;
+}
