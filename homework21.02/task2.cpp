1c3b80b2f3c7693b97dc6af5b609b4d70b0df09f
Valerasan <52313973+Valerasan@users.noreply.github.com>
2020-02-21 18:47:18 +0200

    homework21.02

--- homework21.02/task2.cpp
+++ homework21.02/task2.cpp
@@ -8,15 +8,15 @@ int main()
     int S_time = 0;
     std::cout<<"Enter the time: "<<std::endl;
     std::cin>>S_time;
-    std::cout<<"--------------------------------------"<<std::endl;
+    std::cout<<"---------------------------------------"<<std::endl;
     std::cout<<"Количество отрезков   = "<<CONSTANTA()<<std::endl;
-    std::cout<<"--------------------------------------"<<std::endl;
+    std::cout<<"---------------------------------------"<<std::endl;
     for(int i = 0; i<CONSTANTA(); i++)
     {
         std::cout<<"Отрезок ["<<i<<"] = "<<line(i)<<std::endl;
     }
-    std::cout<<"--------------------------------------"<<std::endl;
-    std::cout<<"--------------------------------------"<<std::endl;
+    std::cout<<"---------------------------------------"<<std::endl;
+    std::cout<<"---------------------------------------"<<std::endl;
     std::cout<<"Общая длина отрезков = "<<lineLenght()<<std::endl;
     std::cout<<"Скорость             = "<<speed(S_time)<<std::endl;
 
